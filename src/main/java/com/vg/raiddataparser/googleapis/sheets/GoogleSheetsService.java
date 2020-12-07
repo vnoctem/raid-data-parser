345202cee41c1fc19ddd051d4b89cd1f2f06ceb8
Vincent Gauthier <vincent.ngo.gauthier@gmail.com>
2020-12-06 22:04:49 -0500

    Remove and ignore Oauth 2.0 tokens

---
+++ src/main/java/com/vg/raiddataparser/googleapis/sheets/GoogleSheetsService.java
@@ -0,0 +1,31 @@
+package com.vg.raiddataparser.googleapis.sheets;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
+import com.vg.raiddataparser.googleapis.GoogleServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class GoogleSheetsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GoogleSheetsService.class.getName());
+    private final Sheets sheetsService = GoogleServiceUtil.getSheetsService();
+
+    public GoogleSheetsService() {
+        LOGGER.info("Initializing GoogleSheetsService...");
+        if (sheetsService == null) {
+            throw new NullPointerException("Error while initializing GoogleSheetsService: Sheets service is null.");
+        }
+        LOGGER.info("GoogleSheetsService initialized");
+    }
+
+    public Spreadsheet createSpreadsheet(SpreadsheetProperties properties) throws IOException {
+        Spreadsheet spreadsheet = new Spreadsheet().setProperties(properties);
+        return Objects.requireNonNull(sheetsService).spreadsheets().create(spreadsheet).execute();
+    }
+
+}
